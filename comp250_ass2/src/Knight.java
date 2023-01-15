03f49cd8ad21c5443795db152e248597eefd7762
Sofia Velasquez <97905425+sofiavelasquezsierra@users.noreply.github.com>
2023-01-15 18:59:36 -0500

    Add files via upload

---
+++ comp250_ass2/src/Knight.java
@@ -0,0 +1,25 @@
+public class Knight extends Piece{
+    public Knight(int x, int y, Side side, Board board) {
+        // TODO: Call super constructor
+        super (x, y, side, board);
+    }
+
+    @Override
+    public boolean canMove(int destX, int destY) {
+
+        //implement rule to hop over pieces
+        //TODO: Check piecerules.md for the movement rule for this piece :)
+        if ((Math.abs(this.x - destX) == 2 && Math.abs(this.y  - destY) == 1) || (Math.abs(this.x - destX) == 1 && Math.abs(this.y  - destY) == 2))
+        return false;
+         else
+            return true;
+        }
+
+        @Override
+        public String getSymbol() {
+            return this.getSide() == Side.BLACK ? "♞" : "♘";
+        }
+}
+
+
+
