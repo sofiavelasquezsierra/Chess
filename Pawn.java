0d42e1ebb0b70cb4e76348bf5acb14ba81c9890d
Sofia Velasquez <97905425+sofiavelasquezsierra@users.noreply.github.com>
2023-01-15 18:31:52 -0500

    Create Pawn.java

---
+++ Pawn.java
@@ -0,0 +1,32 @@
+//@author Sofia Velasquez Sierra
+
+public class Pawn extends Piece{
+        public Pawn(int x, int y, Side side, Board board) {
+            // TODO: Call super constructor
+            super (x, y, side, board);
+        }
+
+        @Override
+        public boolean canMove(int destX, int destY) {
+            // For the pawn, you are responsible for implementing the rule that they can move
+            // 2 blocks forward when it is their first move. To check if it's a first move,
+            // you can simply check if the pawn is still on its starting square.
+            // Please see the PDF for a complete description of the pawn's movement rules.
+            //TODO: Check piecerules.md for the movement rule for this piece :)
+            if ((this.y == 1 || this.y == 6) && this.x == destX && (Math.abs(this.x - destX) == 0) && Math.abs(this.y - destY) <= 2) {
+                return true;
+            } else if (this.x == destX && (Math.abs(this.y - destY) <= 1)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public String getSymbol() {
+            return this.getSide() == Side.BLACK ? "♟" : "♙";
+        }
+}
+
+
+
